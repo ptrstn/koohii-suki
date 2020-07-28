6e871e7feabe4df0cb52a39b6dad9854ce475280
Peter Stein <peterstein@protonmail.com>
2020-07-28 18:47:56 +0200

    Workaround "AttributeError: 'NoneType' object has no attribute 'text'"
    It seems like some stories do not have the "JsReport" and the "JsStar" html tags in the response.
    As a workaround these fields will simply be set to zero, since they are not important anyways.

--- koohii/stories.py
+++ koohii/stories.py
@@ -1,9 +1,34 @@
+from warnings import warn
+
 from bs4 import BeautifulSoup
 
 from koohii.settings import REQUEST_HEADERS
 from koohii.utils import to_unicode_decimal, to_integer
 
 STORIES_URL = "https://kanji.koohii.com/study/SharedStoriesList"
+
+
+def extract_story(kanji_character, story_div):
+    story = story_div.find("div", {"class": "story"}).text
+
+    try:
+        stars = to_integer(story_div.find("a", {"class": "JsStar"}).text)
+    except AttributeError:
+        warn(f"Unable ot find JsStars for Kanji {kanji_character} story {story}")
+        stars = 0
+
+    try:
+        reports = to_integer(story_div.find("a", {"class": "JsReport"}).text)
+    except AttributeError:
+        warn(f"Unable ot find JsReport for Kanji {kanji_character} story {story}")
+        reports = 0
+
+    return {
+        "kanji": kanji_character,
+        "story": story,
+        "stars": stars,
+        "reports": reports,
+    }
 
 
 def get_kanji_stories(koohii_session, kanji_character, number_of_stories=10):
@@ -24,12 +49,4 @@ def get_kanji_stories(koohii_session, kanji_character, number_of_stories=10):
     soup = BeautifulSoup(stories_html, "html.parser")
     story_divs = soup.findAll("div", {"class": "sharedstory"})
 
-    return [
-        {
-            "kanji": kanji_character,
-            "story": story_div.find("div", {"class": "story"}).text,
-            "stars": to_integer(story_div.find("a", {"class": "JsStar"}).text),
-            "reports": to_integer(story_div.find("a", {"class": "JsReport"}).text),
-        }
-        for story_div in story_divs
-    ]
+    return [extract_story(kanji_character, story_div) for story_div in story_divs]
--- tests/test_stories.py
+++ tests/test_stories.py
@@ -1,5 +1,11 @@
+import time
+
+from bs4 import BeautifulSoup
+
 from koohii.login import bugmenot_login_to_kanjii_koohi
-from koohii.stories import get_kanji_stories
+from koohii.settings import REQUEST_HEADERS
+from koohii.stories import get_kanji_stories, STORIES_URL
+from koohii.utils import to_unicode_decimal, to_integer
 
 
 def test_get_kanji_stories():
@@ -20,3 +26,9 @@ def test_get_kanji_stories():
     assert stories[0].get("story")
     assert stories[0].get("stars") >= 0
     assert stories[0].get("reports") >= 0
+
+    time.sleep(2)
+
+    kanji_character = "衣"
+    stories = get_kanji_stories(koohii_session, kanji_character,)
+    assert len(stories) == 10
